75e910510ae0760c63e064468dd2d3d6341e3020
VYOM KUSHVAHA <vyomkushvaha@gmail.com>
2026-08-11 23:59:12 +0530

    Add infix to postfix conversion in q1.cpp
    Implement infix to postfix conversion using a stack.

--- practical/4/q1.cpp
+++ practical/4/q1.cpp
@@ -1,1 +1,69 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
 
+#define MAX 100
+
+char stack[MAX];
+int top = -1;
+
+void push(char ch) {
+    stack[++top] = ch;
+}
+
+char pop() {
+    return stack[top--];
+}
+
+char peek() {
+    return stack[top];
+}
+
+int precedence(char ch) {
+    switch (ch) {
+        case '^': return 3;
+        case '*':
+        case '/': return 2;
+        case '+':
+        case '-': return 1;
+        default: return 0;
+    }
+}
+
+int main() {
+    char infix[MAX], postfix[MAX];
+    int i, j = 0;
+
+    printf("Enter infix expression: ");
+    scanf("%s", infix);
+
+    for (i = 0; infix[i] != '\0'; i++) {
+        char ch = infix[i];
+
+        if (isalnum(ch)) {
+            postfix[j++] = ch;
+        }
+        else if (ch == '(') {
+            push(ch);
+        }
+        else if (ch == ')') {
+            while (top != -1 && peek() != '(')
+                postfix[j++] = pop();
+            pop(); // Remove '('
+        }
+        else {
+            while (top != -1 && precedence(peek()) >= precedence(ch))
+                postfix[j++] = pop();
+            push(ch);
+        }
+    }
+
+    while (top != -1)
+        postfix[j++] = pop();
+
+    postfix[j] = '\0';
+
+    printf("Postfix Expression: %s\n", postfix);
+
+    return 0;
+}
